c83043034f8c0227c055dc3e2996e2d787a124ce
khosta77 <puwerfulpants@gmail.com>
2024-08-09 23:47:03 +0300

    Радиакально улучшение сервера, без доков

--- MultiServer/project/main.cpp
+++ MultiServer/project/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <set>
 #include <map>
 #include <fstream>
 #include <cmath>
@@ -14,9 +15,11 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 std::atomic<size_t> i = 0;
 
@@ -39,36 +42,12 @@ public:
 class Server
 {
 private:
-    //std::vector<int> sock;
-    int listener;
-    struct sockaddr_in addr;
-
-    void socketInit()
-    {
-        listener = socket( AF_INET, SOCK_STREAM, 0 );
-        if( listener < 0 )
-            throw ServerException( ( "listener = " + std::to_string(listener) ) );
-
-    }
-
-    void fillAddress( const std::string& IP, const int& PORT )
-    {
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(PORT);
-        addr.sin_addr.s_addr = inet_addr(IP.c_str());
-    }
-
-    void bindInit()
-    {
-        int b = bind( listener, ( struct sockaddr* )&addr, sizeof(addr) );
-        if( b < 0 )
-            throw ServerException( ( "bind = " + std::to_string(b) ) );
-        listen( listener, 1 );
-    }
+    int _listener;
+    struct sockaddr_in _addr;
 
     int sockAccept()
     {
-        int sock = accept( listener, NULL, NULL );
+        int sock = accept( _listener, NULL, NULL );
         if( sock < 0 )
             throw ServerException( "Sock no accept" );
         return sock;
@@ -126,20 +105,73 @@ private:
 public:
     Server( const std::string& IP, const int& PORT )
     {
-        socketInit();
-        fillAddress( IP, PORT );
-        bindInit();
+        _listener = socket( AF_INET, SOCK_STREAM, 0 );
+        if( _listener < 0 )
+            throw ServerException( ( "listener = " + std::to_string(_listener) ) );
+        fcntl( _listener, F_SETFL, O_NONBLOCK );
+
+        _addr.sin_family = AF_INET;
+        _addr.sin_port = htons(PORT);
+        _addr.sin_addr.s_addr = inet_addr(IP.c_str());
+
+        if( int _bind = bind( _listener, ( struct sockaddr* )&_addr, sizeof(_addr) ); _bind < 0 )
+            throw ServerException( ( "bind = " + std::to_string(_bind) ) );
+        listen( _listener, 1 );
+
         std::cout << "server in system address: " << IP << ":" << PORT << std::endl;
     }
 
     int run()
     {
-        for( ;; )
+        std::set<int> clients;
+
+        while( true )
         {
-            int sock = sockAccept();
-            std::cout << "--->sock:= " << sock << std::endl;
-            std::thread t( &Server::process, this, sock );
-            t.detach(); 
+            // Заполняем множество сокетов
+            fd_set readset;
+            FD_ZERO( &readset );
+            FD_SET( _listener, &readset );
+
+            for( auto it = clients.begin(); it != clients.end(); it++ )
+                FD_SET( *it, &readset );
+
+            // Задаём таймаут
+            timeval timeout;
+            timeout.tv_sec = 15;
+            timeout.tv_usec = 0;
+
+            // Ждём события в одном из сокетов
+            int mx = std::max( _listener, *max_element( clients.begin(), clients.end() ) );
+            if( int s = select( ( mx + 1 ), &readset, NULL, NULL, &timeout ); s <= 0 )  // Вот тут собака зарыта
+                throw ServerException( ( "select " + std::to_string(s) ) );
+
+            // Определяем тип события и выполняем соответствующие действия
+            if( FD_ISSET( _listener, &readset ) )
+            {
+                if( int sock = accept( _listener, NULL, NULL ); sock >= 0 )
+                {
+                    fcntl( sock, F_SETFL, O_NONBLOCK );
+                    clients.insert(sock);
+                }
+                else
+                    throw ServerException( "Sock no accept" );
+            }
+
+            for( auto it = clients.begin(); it != clients.end(); it++ )
+            {
+                if( FD_ISSET( *it, &readset ) )
+                {
+                    std::string content = readFromRecv( *it );
+                    if( content.empty() )
+                    {
+                        std::cout << "socket " << *it << " free" << std::endl;
+                        close(*it);
+                        clients.erase(*it);
+                        break;
+                    }
+                    sendToSock( *it, ( std::to_string(i++) + "|:" + content ) );
+                }
+            }
         }
         return 0;
     }
